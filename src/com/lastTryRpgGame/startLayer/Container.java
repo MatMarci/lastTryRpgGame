7982a1795b4e62fea2c8db5a6b453a049603861a
matDev <mat.a.marcinkowski@gmail.com>
2020-01-02 23:10:32 +0100

    Initial commit

---
+++ src/com/lastTryRpgGame/startLayer/Container.java
@@ -0,0 +1,9 @@
+package com.lastTryRpgGame.startLayer;
+
+public class Container {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, First RPG Game !!!");
+    }
+
+}
