5aa7068b82e66ef06e41c58f9a35910c40a8c8db
Mateusz z DC CPU <mateuszmarcinkowski91@gmail.com>
2020-01-10 20:48:13 +0100

    initial commit

--- src/com/lastTryRpgGame/startLayer/Container.java
+++ src/com/lastTryRpgGame/startLayer/Container.java
@@ -3,7 +3,9 @@ package com.lastTryRpgGame.startLayer;
 public class Container {
 
     public static void main(String[] args) {
+
         System.out.println("Hello, First RPG Game !!!");
+
     }
 
 }
